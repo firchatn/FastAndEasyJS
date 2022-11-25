9c5e6f14fbaa3c0c5c711a32818d57b223020b30
firchatn <firaschaabencss@gmail.com>
2022-11-25 16:23:28 +0100

    session update

--- src/authentification.js
+++ src/authentification.js
@@ -3,6 +3,15 @@ function onSession(id, username) {
   localStorage.setItem("usernameU", username);
 }
 
+function offSession() {
+  localStorage.removeItem("idU");
+  localStorage.removeItem("usernameU");
+}
+
+function singout() {
+  offSession();
+}
+
 function singin(url) {
   var login = document.getElementById("login").value;
   var password = document.getElementById("password").value;
@@ -52,20 +61,17 @@ function singup(url) {
 
 function getUser() {
   var id = localStorage.getItem("idU");
-
-  // Post a user
-  var url3 = "http://127.0.0.1:3000/users/" + id;
-  var xhr3 = new XMLHttpRequest();
-  xhr3.open("GET", url3, true);
-
-  xhr3.onload = function () {
-    var admins = JSON.parse(xhr3.responseText);
-    if (xhr3.readyState == 4 && xhr3.status == "200") {
-      document.getElementById("username").value = admins.username;
-      document.getElementById("phone").value = admins.phone;
-      document.getElementById("email").value = admins.email;
+  var url = "http://127.0.0.1:3000/users/" + id;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    var responses = JSON.parse(xhr3.responseText);
+    if (xhr.readyState == 4 && xhr.status == "200") {
+      document.getElementById("username").value = responses.username;
+      document.getElementById("phone").value = responses.phone;
+      document.getElementById("email").value = responses.email;
     }
   };
 
-  xhr3.send(null);
+  xhr.send(null);
 }
